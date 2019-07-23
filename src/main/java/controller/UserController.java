558db8a827e8b8f552d1dccb3b424370e97b30d6
lyz <289373274@qq.com>
2019-07-23 10:28:08 +0800

    gradle入门

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class UserController {
+   private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
