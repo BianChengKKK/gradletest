afc95e615b358b4318ff6f6bcedb5c37545bb736
lyz <289373274@qq.com>
2019-07-23 10:44:37 +0800

    添加忽略文件

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,4 +1,16 @@
 package controller;
 
 public class OrderController {
+
+  private String orderName;
+
+  public String getOrderName() {
+    return orderName;
+  }
+
+  public void setOrderName(String orderName) {
+    this.orderName = orderName;
+  }
+
+
 }
