e442b6fc740f91f263a4e30e413e14df37f9f474
lyz <289373274@qq.com>
2019-07-23 11:30:51 +0800

    实现了购物车功能

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -1,4 +1,14 @@
 package controller;
 
 public class CartController {
+
+  private String orderName;
+
+  public String getOrderName() {
+    return orderName;
+  }
+
+  public void setOrderName(String orderName) {
+    this.orderName = orderName;
+  }
 }
--- src/main/java/controller/PayController.java
+++ src/main/java/controller/PayController.java
@@ -1,4 +1,14 @@
 package controller;
 
 public class PayController {
+
+  private String orderName;
+
+  public String getOrderName() {
+    return orderName;
+  }
+
+  public void setOrderName(String orderName) {
+    this.orderName = orderName;
+  }
 }
